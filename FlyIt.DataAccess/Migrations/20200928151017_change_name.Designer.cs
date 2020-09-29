d1331e509cb4064324b9e59f2dc62f1732928e85
gundarsv <267009@viauc.dk>
2020-09-29 21:29:58 +0200

    Added flight object

--- FlyIt.DataAccess/Migrations/20200928151017_change_name.Designer.cs
+++ FlyIt.DataAccess/Migrations/20200928151017_change_name.Designer.cs
@@ -4,14 +4,16 @@ using FlyIt.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FlyIt.DataAccess.Migrations
 {
     [DbContext(typeof(IdentityContext))]
-    partial class IdentityContextModelSnapshot : ModelSnapshot
+    [Migration("20200928151017_change_name")]
+    partial class change_name
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- FlyIt.Domain/Mappings/AuthenticationMappingProfile.cs
+++ FlyIt.Domain/Mappings/AuthenticationMappingProfile.cs
@@ -11,6 +11,7 @@ namespace FlyIt.Domain.Mappings
         {
             CreateMap<UserToken, AuthenticationToken>()
                 .ForMember(at => at.AccessToken, opt => opt.MapFrom(ut => ut.Value))
+                .ForMember(at => at.RefreshToken, opt => opt.MapFrom(ut => ut.RefreshToken))
                 .ForMember(at => at.ExpiresAt, opt => opt.MapFrom(ut => new DateTimeOffset(ut.AccessTokenExpiration).ToUnixTimeMilliseconds()))
                 .ForAllOtherMembers(x => x.Ignore());
         }
