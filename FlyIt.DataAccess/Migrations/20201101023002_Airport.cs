3c1c8baf15473378d0d0989a1b2f4f319cc9ab8e
Ionut <ionutputinicabz@gmail.com>
2020-11-01 03:31:15 +0100

    Updated migrations for renting company

---
+++ FlyIt.DataAccess/Migrations/20201101023002_Airport.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FlyIt.DataAccess.Migrations
+{
+    public partial class Airport : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RentingCompanyName",
+                table: "Airport",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RentingCompanyPhoneNo",
+                table: "Airport",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RentingCompanyUrl",
+                table: "Airport",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RentingCompanyName",
+                table: "Airport");
+
+            migrationBuilder.DropColumn(
+                name: "RentingCompanyPhoneNo",
+                table: "Airport");
+
+            migrationBuilder.DropColumn(
+                name: "RentingCompanyUrl",
+                table: "Airport");
+        }
+    }
+}
