4c2bebbd7b17666db94b9c5f99e88d52423ed45a
Ionut <ionutputinicabz@gmail.com>
2020-11-10 18:48:34 +0100

    Added migration for ICAO update on airport endpoint

---
+++ FlyIt.DataAccess/Migrations/20201110174813_ChangedAirportEntity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FlyIt.DataAccess.Migrations
+{
+    public partial class ChangedAirportEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Icao",
+                table: "Airport",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Icao",
+                table: "Airport");
+        }
+    }
+}
