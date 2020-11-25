f447bd8a5b6a00fb6619775e4265990e864c503f
gundarsv <267009@viauc.dk>
2020-11-25 23:16:15 +0100

    Added message type mapping

--- FlyIt.Domain/Mappings/ChatroomMessageMap.cs
+++ FlyIt.Domain/Mappings/ChatroomMessageMap.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using FlyIt.DataAccess.Entities;
 using FlyIt.Domain.Models;
+using FlyIt.Domain.Models.Enums;
 
 namespace FlyIt.Domain.Mappings
 {
@@ -11,7 +12,8 @@ namespace FlyIt.Domain.Mappings
             CreateMap<ChatroomMessage, ChatroomMessageDTO>()
                 .ForMember(crmdto => crmdto.UserName, options => options.MapFrom(crm => crm.User.UserName))
                 .ForMember(crmdto => crmdto.Message, options => options.MapFrom(crm => crm.Message))
-                .ForMember(crmdto => crmdto.DateTime, options => options.MapFrom(crm => crm.DateTime));
+                .ForMember(crmdto => crmdto.DateTime, options => options.MapFrom(crm => crm.DateTime))
+                .ForMember(crmdto => crmdto.MessageType, options => options.MapFrom(crm => MessageType.Message));
         }
     }
 }
